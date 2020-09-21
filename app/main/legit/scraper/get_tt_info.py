6bf908c21c6e4f94a50877270a40234e38d981d5
c3a <anon_c3a@outlook.com>
2020-09-21 17:03:09 +0300

    added tt and sc scrapers

--- app/main/legit/scraper/get_tt_info.py
+++ app/main/legit/scraper/get_tt_info.py
@@ -1,68 +1,20 @@
-from datetime import datetime
 from pprint import pprint
-import requests
-from bs4 import BeautifulSoup
-def get_info(username):
-    resp = requests.get(f'https://www.tiktok.com/@{username}')
-    soup = BeautifulSoup(resp.content)
-    print(resp.content)
-    following = soup.find_all("strong")
+from TikTokApi import TikTokApi
 
-    print(following)
-    #<strong title="Following">31</strong>
-    #<strong title="Followers">11M</strong>
-    #<strong title="Likes">142M</strong>
-    #<span style="font-weight: bold;">5,700,000</span>
-    #<span style="font-weight: bold;">48,800,000</span>
-    #<span style="font-weight: bold;">6</span>
-    #<span style="font-weight: bold;">289</span>
-    #https://www.tiktok.com/@gordonramsayofficial?lang=en
+api = TikTokApi()
 
-    '''
-    count = 20
-    # count and list all of the posts for a given user with the pager
-    total = 0
+def tt_scrape(username):
+    data = api.byUsername(username)[0]
+    resp = data["author"]
+    resp["authorStats"] = data["authorStats"]
 
-    pager = api.getUserPager(username, page_size=count)
+    # resp["id"] = data["id"]
+    # resp["username"] = username
+    # resp["desc"] = data["desc"]
+    # resp["createTime"]= data["createTime"]
 
-    for page in pager:
-        total += len(page)
-
-    print('{} has {} posts'.format(username, total))
-    user = api.getUserObject(username)
-    user['total_posts'] = total
-    return user
-    '''
-get_info('fcbarcelona')
-
-# List all of the posts for a given user after a certain date
+    pprint(resp)
+    return resp
 
 
-
-
-
-
-'''
-api = TikTokPy()
-search = api.search_user(text="fcbarcelona")
-print(search)
-
-
-
-
-user = api.getUserObject("fcbarcelona")
-print(dir(user))
-print('#############################################')
-print(user)
-
-=import requests
-
-def download_no_wm(url, get_bytes=1):
-    url = url.split("?")[0]
-    base_url = "https://savevideo.ninja/wp-admin/admin-ajax.php?action=wppress_tt_download&url={}&key=no-watermark".format(url)
-    if get_bytes == 1:
-        r = requests.get(base_url)
-        return r.content
-    else:
-        return base_url
-'''
+tt_scrape("addisonre")
