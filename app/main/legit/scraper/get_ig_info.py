93eed43f29c07a5538449cbe64c29a15653bd707
cswil <cswilson@zoho.com>
2020-09-22 21:35:19 +0100

    temp fix for scraper

--- app/main/legit/scraper/get_ig_info.py
+++ app/main/legit/scraper/get_ig_info.py
@@ -3,16 +3,30 @@ from bs4 import BeautifulSoup
 import json
 from pprint import pprint
 import random
-def insta_scrape(user):
 
-    profile = requests.get(f"https://www.instagram.com/{user}", headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36'})
-    soup = BeautifulSoup(profile.text, 'html.parser')
-    #print(profile.text)
-    more_data = soup.find_all('script', attrs={'type': 'text/javascript'})
-    data = json.loads(str(more_data[3])[52:-10])['entry_data']['ProfilePage'][0]['graphql']['user']
-    print(data)
-    with open('out', 'w') as f:
-        json.dump(data, f)
+def insta_scrape(username):
+    response = requests.get(f"https://www.instagram.com/{username}")
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+    else:
+        return "User doesnt exist"
+    
+    scripts = soup.find_all('script')
+
+    for s in scripts:
+        try:
+            content = s.contents[0]
+            data_object = content[content.find('{"config"') : -1]
+
+            # check if object exists
+            if data_object != '':
+                data_json = json.loads(data_object)
+                break
+        except Exception as e:
+            print(f'Error has occurred: {e}')
+
+    data = data_json['entry_data']['ProfilePage'][0]['graphql']['user']
+
     followers = data["edge_followed_by"]["count"]
     following = data["edge_follow"]["count"]
     posts = data["edge_owner_to_timeline_media"]["count"]
@@ -22,4 +36,3 @@ def insta_scrape(user):
     engagement = str(random.random() * 100)[0:3]
     return followers, following, posts, bio, pfp_url, username, engagement
 
-insta_scrape('fcbarcelona')
